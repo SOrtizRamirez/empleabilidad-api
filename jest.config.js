5c4d52ee374b68138c45c6fc1bc8cbbb7730073f
Sharon <123331215+SOrtizRamirez@users.noreply.github.com>
2026-01-28 08:38:48 -0500

    Add Jest configuration file for testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  testEnvironment: 'node',
+};
